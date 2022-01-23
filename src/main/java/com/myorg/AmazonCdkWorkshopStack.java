2782970471b64894ac51cffb66348fa4479fd32b
Neil Brown <neil.brown.git@gmail.com>
2022-01-23 21:43:02 +0000

    Fully implement the app’s, previously stubbed, CDK Stack. The AmazonCdkWorkshopStack class now provisions all of the resources required by the AWS serverless app including an AWS API Gateway endpoint, a backend Lambda function, a ‘HitCounter’ (comprising a Lambda function and DynamoDB table), and finally a (DynamoDB) ‘TableViewer’.

--- src/main/java/com/myorg/AmazonCdkWorkshopStack.java
+++ src/main/java/com/myorg/AmazonCdkWorkshopStack.java
@@ -1,29 +1,73 @@
 package com.myorg;
 
+import com.github.eladb.dynamotableviewer.TableViewer;
+
 import software.amazon.awscdk.core.Construct;
-import software.amazon.awscdk.core.Duration;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
-import software.amazon.awscdk.services.sns.Topic;
-import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
-import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.apigateway.LambdaRestApi;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
 
+/**
+ * An Amazon {@link #Stack CDK Stack} (code representation of a single CloudFormation Stack) that declares the bunch of
+ * AWS resources that need to be provisioned for this app, using CloudFormation.
+ */
 public class AmazonCdkWorkshopStack extends Stack {
+
+    private static final String HIT_COUNT_TABLE_HITS_COL_NAME = "hits";
+
+    // Prefix TableViewer construct uses to denote sort order of descending (highest to lowest)
+    private static final String TABLEVIEWER_SORT_ORDER_PREFIX_DESC = "-";
+
+    /**
+     * Creates an instance of this Stack for the specified parent (app) and with the specified construct ID, using the
+     * default Stack properties.
+     *
+     * @see AmazonCdkWorkshopStack#AmazonCdkWorkshopStack(Construct, String, StackProps)
+     */
     public AmazonCdkWorkshopStack(final Construct parent, final String id) {
         this(parent, id, null);
     }
 
+    /**
+     * Creates  an instance of this Stack for the specified parent (app), with the specified construct ID, and using the
+     * specified {@link StackProps Stack Properties}
+     *
+     * @see Stack#Stack(software.constructs.Construct, String, StackProps)
+     */
     public AmazonCdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
+        // Initialise this Stack in the standard way
         super(parent, id, props);
 
-        final Queue queue = Queue.Builder.create(this, "AmazonCdkWorkshopQueue")
-                .visibilityTimeout(Duration.seconds(300))
+        // Add the Lambda function to this app's stack that handles requests to the app's API endpoint
+        final Function helloLambdaFunction = Function.Builder.create(this, "HelloHandler")
+                // Lambda runtime used by the function
+                .runtime(Runtime.NODEJS_14_X)
+                // Path of folder containing function code (relative to where cdk executed from, project’s root dir)
+                .code(Code.fromAsset("lambda"))
+                // Lambda function contained in file "hello" and named "handler"
+                .handler("hello.handler")
                 .build();
 
-        final Topic topic = Topic.Builder.create(this, "AmazonCdkWorkshopTopic")
-            .displayName("My First Topic Yeah")
-            .build();
+        // Provision the set of AWS resources required by  a 'HitCounter' using a custom construct
+        final HitCounter helloFunctionWithHitCounter = new HitCounter(this, "HitCounter",
+            HitCounterProps.builder().downstream(helloLambdaFunction).build()
+        );
 
-        topic.addSubscription(new SqsSubscription(queue));
+        // Declare an API Gateway REST API that proxies the 'hello' Lambda function
+        final LambdaRestApi lambdaRestApi = LambdaRestApi.Builder.create(this, "Endpoint")
+                // Route the request to the HitCounter function that proxies the Hello function
+                .handler(helloFunctionWithHitCounter.getHandler())
+                .build();
+
+        // Provision the set of AWS resources required to support a (DynamoDB) 'TableViewer', using a third party CDK
+        // construct
+        TableViewer.Builder.create(this, "ViewerHitCount")
+                .title("Hello Hits")
+                .table(helloFunctionWithHitCounter.getTable())
+                .sortBy(TABLEVIEWER_SORT_ORDER_PREFIX_DESC + HIT_COUNT_TABLE_HITS_COL_NAME)
+                .build();
     }
 }
