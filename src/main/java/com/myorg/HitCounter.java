ee31d09e2540410b0011b619b2af6452b0523f29
Neil Brown <neil.brown.git@gmail.com>
2022-01-23 21:15:24 +0000

    Add a custom CDK construct that provisions the AWS resources required to support the ‘HitCounter’ - a Lambda function backend for an API, which maintains a count a count of no. of requests, per request path. Provisions the supporting Lambda function and DynamoDB  table.

---
+++ src/main/java/com/myorg/HitCounter.java
@@ -0,0 +1,66 @@
+package com.myorg;
+
+import java.util.Map;
+
+import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.services.dynamodb.Attribute;
+import software.amazon.awscdk.services.dynamodb.AttributeType;
+import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+
+/**
+ * A custom CDK {@link Construct} that can be attached to a Lambda function that’s used as a backend for an API Gateway,
+ * which counts the number of requests received for each URL path, and stores it in a DynamoDB table.
+ */
+public class HitCounter extends Construct {
+	private final Function handler;
+	private final Table table;
+
+	/**
+	 * @inheritDoc
+	 */
+	public HitCounter(Construct scope, String id, HitCounterProps props) {
+		super(scope, id);
+
+		// Provision the DynamoDB table for maintaining the count of the no. of requests per path
+		this.table = Table.Builder.create(this, "Hits")
+			// Include a partition key, which is mandatory for every DynamoDB table
+			.partitionKey(Attribute.builder().name("path").type(AttributeType.STRING).build())
+			.build();
+
+		// Provision/deploy the Lambda function that is used to maintain the hit count when each API request is received
+
+		// Create the env variables used by the Lambda function
+		final Map<String, String> environment = Map.of(
+			"HITS_TABLE_NAME", this.table.getTableName(),
+			"DOWNSTREAM_FUNCTION_NAME", props.getDownstream().getFunctionName()
+		);
+		this.handler = Function.Builder.create(this, "HitCounterHandler")
+			.runtime(Runtime.NODEJS_10_X)
+			.handler("hitcounter.handler")
+			.code(Code.fromAsset("lambda"))
+			.environment(environment)
+			.build();
+
+		this.table.grantReadWriteData(this.handler);
+		props.getDownstream().grantInvoke(this.handler);
+	}
+
+	/**
+	 * @return the {@link Function} for the Lambda function that this CDK Construct provisions/deploys to maintain the
+	 * hit count when each API request is received
+	 */
+	Function getHandler() {
+		return this.handler;
+	}
+
+	/**
+	 * @return the {@link Table} for the DynamoDB table that this CDK Construct uses to maintain the count of the no.
+	 * of requests per path.
+	 */
+	Table getTable() {
+		return this.table;
+	}
+}
---
+++ src/main/java/com/myorg/HitCounterProps.java
@@ -0,0 +1,54 @@
+package com.myorg;
+
+import software.amazon.awscdk.services.lambda.IFunction;
+
+/**
+ * Holds the properties used to create an instance of {@link HitCounter}.
+ */
+// TODO - This class is currently only used to provide a single property to the HitCounter class. Review whether it's
+//  needed. HitCounter could just be changed to accept the IFunction? If it is retained consider simplifying the
+//  class. Is the use of the build pattern warranted? And does it need to be an interface? Could refactor to a class
+//  with a single arg constructor and getter method.
+public interface HitCounterProps {
+
+	/** @return a {@link Builder} for creating an instance of {@link HitCounterProps}. */
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	/** @return the Lambda function which this HitCounter proxies requests for, in order to count the 'hits'. */
+	IFunction getDownstream();
+
+	/** Supports building an instance of a {@link HitCounterProps}. */
+	public static class Builder {
+		private IFunction downstream;
+
+		/**
+		 * @param function the {@link IFunction Lambda function} to use to create the HitCounterProps.
+		 * @return this builder.
+		 */
+		public Builder downstream(final IFunction function) {
+			this.downstream = function;
+			return this;
+		}
+
+		/**
+		 * Builds the {@link HitCounter} from the state provided in previous builder methods.
+		 *
+		 * @return the {@link HitCounter} .
+		 */
+		public HitCounterProps build() {
+			if(this.downstream == null) {
+				throw new NullPointerException("The downstream property is required!");
+			}
+
+			// Create and return and anonymous implementation of HitCounterProps that uses the instance of supplied
+			// downstream property
+			return new HitCounterProps() {
+				public IFunction getDownstream() {
+					return downstream;
+				}
+			};
+		}
+	}
+}
