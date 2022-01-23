55cf9e36fe27ade72cb0667594f8f1b69f54ca3f
Neil Brown <neil.brown.git@gmail.com>
2022-01-23 21:55:19 +0000

    Update unit test for AmazonCdkWorkshopStack to assert the revised expected set of AWS resources that should now be provisioned by the stack, after it was updated to provision the full set of AWS resources required by the app.

--- src/test/java/com/myorg/AmazonCdkWorkshopStackTest.java
+++ src/test/java/com/myorg/AmazonCdkWorkshopStackTest.java
@@ -1,27 +1,44 @@
 package com.myorg;
 
 import software.amazon.awscdk.core.App;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Map;
+
+/**
+ * Tests {@link AmazonCdkWorkshopStack}.
+ */
 public class AmazonCdkWorkshopStackTest {
-    private final static ObjectMapper JSON =
-        new ObjectMapper().configure(SerializationFeature.INDENT_OUTPUT, true);
 
+    private final ObjectMapper objectMapper = new ObjectMapper().configure(SerializationFeature.INDENT_OUTPUT, true);
+
+    /**
+     * Tests an instance of {@link AmazonCdkWorkshopStack} provisions the expected AWS resources required by the app.
+     * <p>
+     * Does so by verify that the CDK app’s synthesising method produces a CloudFormation (JSON) stack template
+     * containing the expected AWS resources.
+     *
+     * @throws Exception if an unexpected error occurs on execution of this test.
+     */
     @Test
-    public void testStack() throws IOException {
+    @SuppressWarnings("unchecked")
+    public void testStack() throws Exception {
         App app = new App();
+
         AmazonCdkWorkshopStack stack = new AmazonCdkWorkshopStack(app, "test");
+        Map<String, Map<String, Object>> cfnTemplateAsMap =
+          (Map<String, Map<String, Object>>) app.synth().getStackArtifact(stack.getArtifactId()).getTemplate();
 
-        JsonNode actual = JSON.valueToTree(app.synth().getStackArtifact(stack.getArtifactId()).getTemplate());
-
-        assertThat(actual.toString())
-            .contains("AWS::SQS::Queue")
-            .contains("AWS::SNS::Topic");
+        String cfnTemplateAsJson = this.objectMapper.valueToTree(cfnTemplateAsMap).toString();
+        // Cursory check that the synthesised stack contains a selection of the expected types of AWS resources
+        assertThat(cfnTemplateAsJson)
+          .contains("AWS::IAM::Role")
+          .contains("AWS::Lambda::Function")
+          .contains("AWS::DynamoDB::Table")
+          .contains("AWS::ApiGateway::RestApi");
     }
 }
